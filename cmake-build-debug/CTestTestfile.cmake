542f2739968aaae39604d98b6b6bdcc1df9fef6e
Bytenol <ccosmos416@gmail.com>
2024-07-27 13:12:22 -0400

    pong2d init

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/bytenol/Documents/programming/Github/Dl2Hub
-# Build directory: /home/bytenol/Documents/programming/Github/Dl2Hub/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("deps/SDL")
-subdirs("example/SimpleGames")
